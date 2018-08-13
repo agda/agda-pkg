3eb0ef4ec81e76b5c59840ba0acd312f21203ef4
apkgbot <apkgbot@gmail.com>
2018-08-13 23:08:17 +1000

    [Upgrade] added command upgrade and information about pkg version

--- src/pkg.py
+++ src/pkg.py
@@ -5,14 +5,18 @@ from .commands.info import info
 from .commands.install import install
 from .commands.search import search
 from .commands.update import update
+from .commands.upgrade import upgrade
 
 
 @click.group()
+@click.version_option()
 def cli():
   pass
+  
 
 cli.add_command(install)
 cli.add_command(info)
 cli.add_command(search)
 cli.add_command(update)
 cli.add_command(download)
+cli.add_command(upgrade)
