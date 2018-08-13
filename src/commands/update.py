f71179bb6499d0d5c6509fc12b229bfce58a6f9f
apkgbot <apkgbot@gmail.com>
2018-08-13 21:48:13 +1000

    [Update] added command update for a specific package

---
+++ src/commands/update.py
@@ -0,0 +1,13 @@
+import click
+
+
+@click.group()
+def update():
+	pass
+
+@update.command()
+@click.argument('name')
+@click.option('--package', '-p', type=float, default=-1.0)
+def update(name, package):
+    click.echo('%s' % name)
+    click.echo(package)
--- src/pkg.py
+++ src/pkg.py
@@ -2,6 +2,7 @@ import click
 from .commands.install import install
 from .commands.info import info
 from .commands.search import search
+from .commands.update import update
 
 
 @click.group()
@@ -12,3 +13,4 @@ def cli():
 cli.add_command(install)
 cli.add_command(info)
 cli.add_command(search)
+cli.add_command(update)
