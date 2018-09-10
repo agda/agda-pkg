f93f3a9cb4d76f9b11c72898158b874279d2c8a8
Jonathan Prieto-Cubides <jcu043@uib.no>
2018-09-10 10:30:09 +0200

    [ requirements.txt ] simplified

--- src/commands/install.py
+++ src/commands/install.py
@@ -10,6 +10,10 @@ def install():
   click.echo('[Clone]: initializing cloning')
   click.echo('[Clone]: repository successfully cloned')
 
+# @install.command()
+# def install():
+#   click.echo('This is the zone subcommand of the install command')
+
 @install.command()
 def local():
   click.echo('This is the zone subcommand of the install command')
