988b3f3ef3865b8aad4bed828c81784ea3c323c8
Jonathan Prieto-Cubides <jcu043@uib.no>
2018-10-03 13:26:21 +0200

    [ v0.1.20 ] new version: travis fixing

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*names, **kwargs):
 
 setup(
     name='agda-pkg'
-  , version="0.1.19"
+  , version="0.1.20"
   , python_requires='>=3.6.0'
   , url='https://github.com/apkgbot/agda-pkg'
   , license='MIT'
