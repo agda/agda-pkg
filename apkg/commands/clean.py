3cbdad399960b219149a4601a8681783aa2c9a5c
Jonathan Prieto-Cubides <jonathan.cubides@uib.no>
2020-01-29 11:55:45 +0100

    [ apkg clean ] --yes flag used to bypass the prompt

--- apkg/commands/clean.py
+++ apkg/commands/clean.py
@@ -18,20 +18,24 @@ from ..service.logging   import logger, clog
 
 # ----------------------------------------------------------------------------
 
+rmdirs = [ AGDA_PKG_PATH , AGDA_DIR_PATH ]
+msg1 = ('\n  '.join(map(lambda x : x.as_posix(), rmdirs)))
+promptMessage = 'Directories:\n  ' + msg1 + '\nDo you really want to remove these directories?'
+
 # -- Command def.
 @click.group()
 def clean(): pass
 
 @clean.command()
+@click.confirmation_option(prompt=promptMessage)
 @clog.simple_verbosity_option(logger)
 def clean():
   """Remove the directories used by Agda-Pkg."""
-  rmdirs = [ AGDA_PKG_PATH , AGDA_DIR_PATH ]
+
   for dir in rmdirs:
     namedir = dir.as_posix()
-    if click.confirm('Do you really want to remove the directory tree'):
-      try:
-        shutil.rmtree(dir)
-        logger.info(dir.as_posix() + " (deleted)")
-      except Exception as e:
-        logger.error(e)
+    try:
+      shutil.rmtree(dir)
+      logger.info(dir.as_posix() + " (deleted)")
+    except Exception as e:
+      logger.error(e)
